d0b92beecd94f446f2e08faac5b6150029ba3b9b
Memesh <memesh@MacBook-Air-de-Memesh.local>
2026-04-13 16:18:20 -0500

    test env variable

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def details():
     return jsonify({
         "time": datetime.date.today().strftime("%B %d, %Y"),
         "hostname": socket.gethostname(),
-        "message": "You are doing great human"
+        "message": "You are doing great human."
     })
 
 
